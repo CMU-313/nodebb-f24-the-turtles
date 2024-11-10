e0ea1e31cf0441f685de4cb3ba3a3c114e64666f
Shanting Hou <shantinghhou@gmail.com>
2024-11-10 11:56:29 -0500

    update translator api

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,7 +4,7 @@ const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
     // Edit the translator URL below
-    const TRANSLATOR_API = "https://nodebb-f24-translator.azurewebsites.net/"
+    const TRANSLATOR_API = "https://turtles-translator-service-c2a5g7f4c2gsbbf9.eastus-01.azurewebsites.net/"
     const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
     const data = await response.json();
     return [data["is_english"], data["translated_content"]]
