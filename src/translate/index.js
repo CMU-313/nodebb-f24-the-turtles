67232e53d045fd299584bc3622ffec2582f5a79c
Shanting Hou <shantinghhou@gmail.com>
2024-11-10 12:06:50 -0500

    jshint fix

--- src/translate/index.js
+++ src/translate/index.js
@@ -1,11 +1,12 @@
+/* jshint unused:false */
 var request = require('request');
 
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
     // Edit the translator URL below
-    const TRANSLATOR_API = "https://turtles-translator-service-c2a5g7f4c2gsbbf9.eastus-01.azurewebsites.net/"
+    const TRANSLATOR_API = "https://turtles-translator-service-c2a5g7f4c2gsbbf9.eastus-01.azurewebsites.net/";
     const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
     const data = await response.json();
-    return [data["is_english"], data["translated_content"]]
-}
+    return [data["is_english"], data["translated_content"]]; // jshint ignore:line
+};
